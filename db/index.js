8abaf9fd5c36f2f7867975571a7aac61452aba82
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-04-23 20:36:08 -0700

    Work on login route

--- db/index.js
+++ db/index.js
@@ -1,14 +1,15 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-user: "labber", 
-password: "labber",
-host: "localhost",
-database: "wikimaps"
-})
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "wikimaps",
+  port: 5432,
+});
 
 const query = (query, params, callback) => {
   return pool.query(query, params, callback);
-}
+};
 
-module.exports = {pool, query};
+module.exports = { pool, query };
