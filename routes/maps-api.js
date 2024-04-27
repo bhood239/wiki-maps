6668a1154ccdd0865bc018f112f3cc7e58ce2967
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-04-26 19:12:44 -0700

    Create route for creating new map

--- routes/maps-api.js
+++ routes/maps-api.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router  = express.Router();
 const mapsQueries = require('../db/queries/maps');
+const db = require('../db/connection');
 
 // PINS CRUD REST API
 
 // CREATE - POST /
 router.post('/', (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: 'map created'
-  })
-})
+  console.log('userid:' + req.session.userId);
+  console.log('cookie:' + req.session.user);
+  if (!req.session.user || !req.session.userId) {
+    res.status(401).send('Not logged in');
+    return;
+  }
+
+  const userId = req.session.userId;
+  const name = req.body.name;
+  const description = req.body.description;
+  const lat = req.body.lat;
+  const lng = req.body.lng;
+
+  return db.query('INSERT INTO maps (creator_id, name, description, lat, lng) VALUES ($1, $2, $3, $4, $5) RETURNING *', [userId, name, description, lat, lng])
+    .then(data => {
+      console.log(data.rows[0]);
+      res.status(200).send('Success');
+      return;
+    });
+});
 
 //READ ALL - GET /
 router.get('/', async (req, res) => {
