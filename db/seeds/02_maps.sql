58f9dd38d48b0c270b13344b4b5cfcf9af7c6460
Ben Hood <benhood97@btinternet.com>
2024-04-27 11:38:29 -0700

    make maps seeds coordinates unique

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -12,27 +12,27 @@ VALUES
     4,
     'Sightseeing in Whistler',
     'Here are some nice places to take photos!',
-    50.1162,
-    -122.9535
+    50.105583,
+    -122.983019
   ),
   (
     5,
     'Best Poutine in Whistler',
     'Poutine restaurants and fast food!',
-    50.1162,
-    -122.9535
+    50.149312,
+    -122.961011
   ),
   (
     3,
     'Skiing in Whistler',
     'This is where you can ski, like Whistler Mountain!',
-    50.1162,
-    -122.9535
+    50.095090,
+    -122.995733
   ),
   (
     4,
     'Grocery Stores in Whistler',
     'This is where I buy groceries.',
-    50.1162,
-    -122.9535
+    50.123840,
+    -122.933577
   );
