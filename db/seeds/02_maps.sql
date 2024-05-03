e8f9d7d15b915854b703781063b1e28d2c1b89cd
Ben Hood <benhood97@btinternet.com>
2024-05-02 19:51:02 -0700

    change map seeds to be less ai generated

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,7 +1,7 @@
 -- Insert seed data for the maps table
 INSERT INTO maps (creator_id, name, description, lat, lng) VALUES
-  (1, 'Nature Trail', 'Explore a scenic nature trail', 48.8588443, 2.2943506),
-  (2, 'City Tour', 'Discover the city landmarks', 40.712776, -74.005974),
-  (3, 'Beach Getaway', 'Relax at a beautiful beach', -33.867487, 151.20699),
-  (1, 'Mountain Adventure', 'Exciting mountain trek', 39.7392358, -104.990251),
-  (3, 'Historical Journey', 'Journey through ancient sites', 51.5073509, -0.1277583);
+  (1, 'Paris Nature Trails', 'Here is how I find nature in the heart of the city!', 48.8588443, 2.2943506),
+  (2, 'New York City Tour', 'See my citys most popular landmarks', 40.712776, -74.005974),
+  (3, 'Sydney Beach Spots', 'My favorite beach spots in Sydney', -33.867487, 151.20699),
+  (1, 'Mountain Adventure Spots', 'Here is some awesome mountain adventures', 39.7392358, -104.990251),
+  (3, 'Historical Journey through London', 'Journey through Londons rich history!', 51.5073509, -0.1277583);
