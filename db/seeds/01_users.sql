695a62ae1a29289dbd2b5e3e10016d2d855d4c2c
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-05-03 18:19:45 -0700

    2 minor typos

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -6,5 +6,5 @@ VALUES
   ('a', 'a', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', '25321jklgjk'),
   ('alice', 'ecila', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', 'f3j21iofxsfb'),
   ('bob', 'bob', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', 'rf342j1klfdsa'),
-  ('carol', 'laroc', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', 'fkbd9s00abk');
+  ('carol', 'laroc', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', 'fkbd9s00abk'),
   ('Victoria', 'victoria', 'https://i.pinimg.com/originals/5c/f9/09/5cf909648efb78e4a13ea3edd84e3bdf.jpg', 'c5bc1e67-59bb-4eef-8e8d-b04a25061900');
