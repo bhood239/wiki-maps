085cb789ceb08038cebb48816b1f9f7b3985a17b
Ben Hood <benhood97@btinternet.com>
2024-05-03 19:19:04 -0700

    "change users seed"

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -7,4 +7,4 @@ VALUES
   ('alice', 'ecila', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', 'f3j21iofxsfb'),
   ('bob', 'bob', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', 'rf342j1klfdsa'),
   ('carol', 'laroc', 'https://i.pinimg.com/originals/1c/d4/da/1cd4dab4a093af8f3988e3d3e98fc8bc.jpg', 'fkbd9s00abk'),
-  ('Victoria', 'victoria', 'https://i.pinimg.com/originals/5c/f9/09/5cf909648efb78e4a13ea3edd84e3bdf.jpg', 'c5bc1e67-59bb-4eef-8e8d-b04a25061900');
+  ('victoria', 'victoria', 'https://i.pinimg.com/originals/5c/f9/09/5cf909648efb78e4a13ea3edd84e3bdf.jpg', 'c5bc1e67-59bb-4eef-8e8d-b04a25061900');
